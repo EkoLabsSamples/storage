e49822102362f775aeaee00839fea80180d7f20a
Asaf Menahem <asaf@helloeko.com>
2018-12-27 14:47:24 +0200

    The actual storage example

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,18 @@
-import playerOptions from '../config/playerOptions.json';
-
 export default {
-    onLoad: function(ctx) { },
-
     onInit: function(player, ctx) {
-        // See https://developer.helloeko.com for API documentation. Place your code here:
-    },
+        let currentDate = new Date().toUTCString();
 
-    playerOptions
+        player.storage.get('storageExampleId', { scope: 'episode' })
+            .then(function(storageProxy) {
+                // If value already exists, show it.
+                let lastDateWatched = storageProxy.getItem('lastDateWatched');
+                if (lastDateWatched) {
+                    alert(`Welcome back! you last watched this episode at:\n${lastDateWatched}`);
+                }
+
+                // Set the new value for the next project playing / new session.
+                storageProxy.setItem('lastDateWatched', currentDate);
+            })
+            .catch(function() { console.log('Error attempting to get storage...'); });
+    }
 };
